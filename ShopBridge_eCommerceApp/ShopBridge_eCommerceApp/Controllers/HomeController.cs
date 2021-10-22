dfd717ad2489faa4a5a95a1c68d336304beaccfc
Soumya S <soumya.s@dxc.com>
2021-10-23 00:37:21 +0530

    Login page for admin done

--- ShopBridge_eCommerceApp/ShopBridge_eCommerceApp/Controllers/HomeController.cs
+++ ShopBridge_eCommerceApp/ShopBridge_eCommerceApp/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ShopBridge_eCommerceApp.Models;
 
 namespace ShopBridge_eCommerceApp.Controllers
 {
     public class HomeController : Controller
     {
+        ThinkBridgeEntities db1 = new ThinkBridgeEntities();
         public ActionResult Index()
         {
             return View();
@@ -25,6 +27,32 @@ namespace ShopBridge_eCommerceApp.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Admin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Admin(ProductAdminLogin tb1)
+        {
+           
+            var check = db1.ProductAdminLogins.Where(x => x.Name.Equals(tb1.Name) && x.Password.Equals(tb1.Password)).FirstOrDefault();
+            if (check != null)
+            {
+                Session["Username"] = tb1.Name.ToString();
+                Session["Password"] = tb1.Password.ToString();
+                ViewBag.name = Session["Username"];
+
+                return RedirectToAction("index");
+            }
+            else
+            {
+                Response.Write("Invalid Name or password!!!");
+            }
+
+            return View();
+
         }
     }
 }
